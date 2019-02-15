ad7ef3a017311e0c67064f2e4067ff3d2defb42d
nickraz <nickraz@yandex.ru>
2019-02-15 15:57:04 +0300

    class Organization added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,16 @@
-import java.math.BigInteger;
-import java.util.Iterator;
-
 public class Main {
 
     public static void main(String[] args) {
-        Fibo fibo = new Fibo();
-        Iterator<BigInteger> iterator = fibo.iterator();
-        for (int i = 0; i < 100; i++) {
-            System.out.println(iterator.next());
-        }
+        Organization organization = new Organization();
+        organization.add("K1\\SK1");
+        organization.add("K1\\SK2");
+        organization.add("K1\\SK1\\SSK1");
+        organization.add("K1\\SK1\\SSK2");
+        organization.add("K2");
+        organization.add("K2\\SK1\\SSK1");
+        organization.add("K2\\SK1\\SSK2");
+
+        organization.printAll(true);
     }
 
 }
